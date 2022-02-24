3610acd9eb4496d0929d67bb1bed2dbcce5da5f2
刘艺华 <i@liuyihua.com>
2022-02-24 09:56:05 +0000

    support export column family and import column family

--- src/db.rs
+++ src/db.rs
@@ -14,6 +14,7 @@
 //
 
 use crate::{
+    checkpoint::ExportImportFilesMetaData,
     column_family::AsColumnFamilyRef,
     column_family::BoundColumnFamily,
     column_family::UnboundColumnFamily,
@@ -2111,6 +2112,30 @@ impl<T: ThreadMode, D: DBInner> DBCommon<T, D> {
         drop(cf);
         Ok(())
     }
+
+    /// Create column family from importing data
+    fn create_column_family_with_import(
+        &self,
+        name: &str,
+        opts: &Options,
+        metadata: &ExportImportFilesMetaData,
+    ) -> Result<*mut ffi::rocksdb_column_family_handle_t, Error> {
+        let cf_name = if let Ok(c) = CString::new(name.as_bytes()) {
+            c
+        } else {
+            return Err(Error::new(
+                "Failed to convert path to CString when creating cf".to_owned(),
+            ));
+        };
+        Ok(unsafe {
+            ffi_try!(ffi::rocksdb_create_column_family_with_import(
+                self.inner.inner(),
+                opts.inner,
+                cf_name.as_ptr(),
+                metadata.inner,
+            ))
+        })
+    }
 }
 
 impl<I: DBInner> DBCommon<SingleThreaded, I> {
@@ -2123,6 +2148,19 @@ impl<I: DBInner> DBCommon<SingleThreaded, I> {
         Ok(())
     }
 
+    /// Create column family from importing data
+    pub fn create_cf_with_import<N: AsRef<str>>(
+        &mut self,
+        name: N,
+        opts: &Options,
+        metadata: &ExportImportFilesMetaData,
+    ) -> Result<(), Error> {
+        let inner = self.create_column_family_with_import(name.as_ref(), opts, metadata)?;
+        self.cfs
+            .cfs
+            .insert(name.as_ref().to_string(), ColumnFamily { inner });
+        Ok(())
+    }
     /// Drops the column family with the given name
     pub fn drop_cf(&mut self, name: &str) -> Result<(), Error> {
         if let Some(cf) = self.cfs.cfs.remove(name) {
@@ -2142,6 +2180,21 @@ impl<I: DBInner> DBCommon<MultiThreaded, I> {
     /// Creates column family with given name and options
     pub fn create_cf<N: AsRef<str>>(&self, name: N, opts: &Options) -> Result<(), Error> {
         let inner = self.create_inner_cf_handle(name.as_ref(), opts)?;
+        self.cfs.cfs.write().unwrap().insert(
+            name.as_ref().to_string(),
+            Arc::new(UnboundColumnFamily { inner }),
+        );
+        Ok(())
+    }
+
+    /// Create column family from importing data
+    pub fn create_cf_with_import<N: AsRef<str>>(
+        &mut self,
+        name: N,
+        opts: &Options,
+        metadata: &ExportImportFilesMetaData,
+    ) -> Result<(), Error> {
+        let inner = self.create_column_family_with_import(name.as_ref(), opts, metadata)?;
         self.cfs.cfs.write().unwrap().insert(
             name.as_ref().to_string(),
             Arc::new(UnboundColumnFamily { inner }),
--- tests/test_column_family.rs
+++ tests/test_column_family.rs
@@ -17,6 +17,7 @@ mod util;
 use pretty_assertions::assert_eq;
 use haizhi_rocksdb as rocksdb;
 
+use rocksdb::checkpoint::{Checkpoint, ExportImportFilesMetaData};
 use rocksdb::{ColumnFamilyDescriptor, MergeOperands, Options, DB, DEFAULT_COLUMN_FAMILY_NAME};
 use rocksdb::{TransactionDB, TransactionDBOptions};
 use util::DBPath;
@@ -498,4 +499,73 @@ fn test_no_leaked_column_family() {
         // make it explicit not to drop the db until we get dir size above...
         drop(db);
     }
+}
+
+#[test]
+fn test_create_cf_with_import() {
+    const PATH_PREFIX: &str = "_rust_rocksdb_create_cf_with_import_";
+
+    // Create DB with some data
+    let origin_db_path = DBPath::new(&format!("{}db1", PATH_PREFIX));
+
+    let mut opts = Options::default();
+    opts.create_if_missing(true);
+    let mut origin_db = DB::open(&opts, &origin_db_path).unwrap();
+    // create two column families
+    assert!(origin_db.create_cf("cf1", &opts).is_ok());
+    assert!(origin_db.create_cf("cf2", &opts).is_ok());
+    assert!(origin_db.cf_handle("cf1").is_some());
+    assert!(origin_db.cf_handle("cf2").is_some());
+
+    assert!(origin_db.put(b"0", b"0").is_ok());
+    let cf1 = origin_db.cf_handle("cf1").unwrap();
+    assert!(origin_db.put_cf(&cf1, b"1", b"1").is_ok());
+    let cf2 = origin_db.cf_handle("cf2").unwrap();
+    assert!(origin_db.put_cf(&cf2, b"2", b"2").is_ok());
+
+    let checkpoint = Checkpoint::new(&origin_db);
+    assert!(checkpoint.is_ok());
+    let checkpoint = checkpoint.unwrap();
+
+    let export_path = DBPath::new(&format!("{}db1_backup", PATH_PREFIX));
+    let result = checkpoint.export_column_family(cf1, &export_path);
+    assert!(result.is_ok());
+    drop(checkpoint);
+
+    let origin_metadata = result.unwrap();
+    let metadata_path = Path::new("/tmp/db1_metadata.json");
+    origin_metadata.save(metadata_path).unwrap();
+    let recover_metadata = ExportImportFilesMetaData::load(metadata_path).unwrap();
+    // new db from export path
+    let recover_db_path = DBPath::new(&format!("{}db1_recover", PATH_PREFIX));
+    let mut recover_db = DB::open(&opts, &recover_db_path).unwrap();
+    assert!(recover_db.cf_handle("cf1").is_none());
+    assert!(recover_db.cf_handle("cf2").is_none());
+    let result = recover_db.create_cf_with_import("cf1", &opts, &recover_metadata);
+    assert!(result.is_ok());
+    assert!(recover_db.cf_handle("cf1").is_some());
+    let cf1 = recover_db.cf_handle("cf1").unwrap();
+    assert_eq!(recover_db.get_cf(&cf1, b"1").unwrap().unwrap(), b"1");
+    assert!(recover_db.cf_handle("cf2").is_none());
+    assert!(recover_db.get_cf(&cf1, b"2").unwrap().is_none());
+    // then we will test origin db
+    assert!(origin_db.put_cf(&cf1, b"11", b"11").is_ok());
+    assert!(origin_db.drop_cf("cf1").is_ok());
+    // cf1 and its data are none
+    assert!(origin_db.cf_handle("cf1").is_none());
+    // import cf1
+    assert!(origin_db
+        .create_cf_with_import("cf1", &opts, &recover_metadata)
+        .is_ok());
+    let cf1 = origin_db.cf_handle("cf1").unwrap();
+    assert_eq!(origin_db.get_cf(&cf1, b"1").unwrap().unwrap(), b"1");
+    assert!(origin_db.get_cf(&cf1, b"11").unwrap().is_none());
+    assert!(origin_db.put_cf(&cf1, b"11", b"11").is_ok());
+    // import cf3
+    assert!(origin_db
+        .create_cf_with_import("cf3", &opts, &recover_metadata)
+        .is_ok());
+    let cf3 = origin_db.cf_handle("cf3").unwrap();
+    assert_eq!(origin_db.get_cf(&cf3, b"1").unwrap().unwrap(), b"1");
+    assert!(origin_db.get_cf(&cf3, b"11").unwrap().is_none());
 }
